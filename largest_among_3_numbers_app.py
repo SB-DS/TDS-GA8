3e32e72fc67884cd46618d3235194e00b27da848
SB-DS <123827883+SB-DS@users.noreply.github.com>
2023-08-31 16:32:48 +0530

    largest_among_3_numbers_app.py

---
+++ largest_among_3_numbers_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def find_largest_number(num1, num2, num3):
+    return max(num1, num2, num3)
+
+st.title("Find the largest among the 3 given numbers.")
+
+# Input
+num1 = st.number_input("Enter the first number: ")
+num2 = st.number_input("Enter the second number: ")
+num3 = st.number_input("Enter the third number: ")
+
+
+# Find and display the largest number
+if st.button("Find Largest"):
+    largest = find_largest_number(num1, num2, num3)
+    st.success(f"The largest number is: {largest}")
