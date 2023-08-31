9b6cf301d4bdc3954ffd53f5d5e1b2140d528b50
SB-DS <123827883+SB-DS@users.noreply.github.com>
2023-08-31 17:57:45 +0530

    Create largest_among_3_numbers_app.py

--- largest_among_3_numbers_app.py
+++ largest_among_3_numbers_app.py
@@ -6,7 +6,7 @@ def find_largest_number(num1, num2, num3):
 st.title("This app is build by Sushobhan Bhargav (22F1000948), for TDS GA-8 submition.\n")
 
 st.title("Find the largest among the 3 numbers.")
-st.title("Choose Your numbers below:")
+st.title("Please, Choose your numbers below:")
 
 # Input
 num1 = st.number_input("Enter the first number: ")
